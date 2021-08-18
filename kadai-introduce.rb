d7cc0e2948ba02ffe310fce429ed0672d15244ff
test.cars <test.cars3388@gmail.com>
2021-08-18 06:24:17 +0000

    Fix hensuu

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,9 +1,9 @@
 #課題　自己紹介文　2021.8.17
 
-Last_name = "〆野"  #苗字
-First_name = "和範" #名前
+last_name = "〆野"  #苗字
+first_name = "和範" #名前
 
-name = Last_name + First_name   #フルネーム
+name = last_name + first_name   #フルネーム
 age = 39    #年齢(2021/8/17現在)
 
 puts name + "です。" + age.to_s + "歳です。"    #自己紹介
